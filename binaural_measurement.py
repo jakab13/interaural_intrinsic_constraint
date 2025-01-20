7731d4fa66749a46a5d3991c6ea36f62bc2f4056
suongwelp <suongwelp@gmail.com>
2025-01-20 18:41:39 +0100

    Plot results

--- binaural_measurement.py
+++ binaural_measurement.py
@@ -8,8 +8,8 @@ import pathlib
 import os
 import matplotlib.pyplot as plt
 
-subject = "kemar"
-azimuth = 45
+subject = "kemar_mic"
+azimuth = 35
 freqs = [400, 500, 600, 700, 800]
 
 SAMPLE_RATE = 48828
@@ -45,15 +45,15 @@ centre_frequency = 500
 low_cutoff = centre_frequency / (2 ** (1 / 6))
 high_cutoff = centre_frequency * (2 ** (1 / 6))
 
-noise_broadband = slab.Sound.whitenoise(duration=.5, level=90).ramp()
 noise_band_limited = slab.Sound.whitenoise(duration=.5, level=90).filter(frequency=(low_cutoff, high_cutoff), kind="bp").ramp()
 tone = slab.Sound.tone(duration=.5, level=90, frequency=centre_frequency).ramp()
 silence = slab.Sound.silence(duration=.5)
 
-sound = noise_broadband
-sound.level = 80
-
 for _ in range(10):
+    noise_broadband = slab.Sound.whitenoise(duration=.5, level=90).ramp()
+    sound = noise_broadband
+    sound.level = 80
+
     rec = play_and_record_local(sound)
     folder = "recordings"
     filename = '_'.join(filter(None, (subject, "azi", str(azimuth))))
@@ -65,18 +65,21 @@ for _ in range(10):
     time.sleep(0.1)
 
 DIR_recs = DIR / "recordings"
+subjects = os.listdir(DIR_recs)
+subjects = [subject]
 
 df_binaural_measurements = pd.DataFrame()
-for subject in os.listdir(DIR_recs):
-    for azimuth in os.listdir(DIR_recs / subject):
-        for i, filename in enumerate(os.listdir(DIR_recs / subject / azimuth)):
-            rec = slab.Binaural(DIR_recs / subject / azimuth / filename)
-            itd = rec.itd() / rec.samplerate
-            azimuth_int = int(azimuth[4:])
+
+for subject in subjects:
+    for azi in os.listdir(DIR_recs / subject):
+        for i, filename in enumerate(os.listdir(DIR_recs / subject / azi)):
+            rec = slab.Binaural(DIR_recs / subject / azi / filename)
+            azimuth_int = int(azi[4:])
             for freq in freqs:
                 low_cutoff = freq / (2 ** (1 / 6))
                 high_cutoff = freq * (2 ** (1 / 6))
                 rec = rec.filter(frequency=(low_cutoff, high_cutoff), kind="bp")
+                itd = rec.itd() / rec.samplerate
                 ild = rec.ild()
                 binaural_current = {
                     "filename": filename,
@@ -90,17 +93,32 @@ for subject in os.listdir(DIR_recs):
                     "rms_right": rec.right.level
                 }
                 df_binaural_measurements = df_binaural_measurements.append(binaural_current, ignore_index=True)
-                print("Computed binaural for", subject, "at", azimuth, "degrees","for rec", i + 1, "at", freq, 'Hz')
+                print("Computed binaural for", subject, "at", azi, "degrees","for rec", i + 1, "at", freq, 'Hz')
 
 
-df_binaural_measurements[df_binaural_measurements.azimuth == 0].groupby(["subject", "freq"])["ild"].mean()
+df_binaural_measurements[df_binaural_measurements.azimuth == 0].groupby(["subject", "freq"])["ild", "itd"].mean()
+df_binaural_measurements.groupby(["subject", "freq", "azimuth"])["ild", "itd"].mean()
+
+g = sns.FacetGrid(data=df_binaural_measurements, row="freq", col="subject", sharey=False)
+g.map(sns.lineplot, "azimuth", "rms_left", marker="o", color="g")
+g.map(sns.lineplot, "azimuth", "rms_right", marker="o", color="r")
+g.map(sns.lineplot, "azimuth", "ild", marker="o", color="b")
+plt.show()
+
+g = sns.FacetGrid(data=df_binaural_measurements, row="freq", col="subject")
+g.map(sns.lineplot, "azimuth", "ild", marker="o", color="b")
+plt.show()
+
+g = sns.FacetGrid(data=df_binaural_measurements, hue="freq", col="subject")
+g.map(sns.lineplot, "azimuth", "itd", marker="o")
+plt.show()
 
 df_sub = df_binaural_measurements[df_binaural_measurements.subject == subject]
 
-fig, ax = plt.subplots(1, 2)
-sns.pointplot(data=df_sub, x="azimuth", y="rms_left", hue="freq", ax=ax[0])
-sns.pointplot(data=df_sub, x="azimuth", y="rms_right", hue="freq", ax=ax[1])
+fig, ax = plt.subplots(5, 2)
+sns.lineplot(data=df_sub, x="azimuth", y="rms_left", hue="freq", ax=ax[0], marker="o")
+sns.lineplot(data=df_sub, x="azimuth", y="rms_right", hue="freq", ax=ax[1], marker="o")
 plt.show()
 
-sns.pointplot(data=df_sub, x="azimuth", y="rms_left", hue="freq")
+sns.lineplot(data=df_sub, x="azimuth", y="ild", hue="freq", marker="o")
 plt.show()

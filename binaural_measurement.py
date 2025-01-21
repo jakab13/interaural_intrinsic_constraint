b8a27b1ce828eb0e1ab3eab7ea6fba70dade7c86
suongwelp <suongwelp@gmail.com>
2025-01-21 17:16:14 +0100

    Record per frequency

--- binaural_measurement.py
+++ binaural_measurement.py
@@ -8,9 +8,9 @@ import pathlib
 import os
 import matplotlib.pyplot as plt
 
-subject = "kemar_mic"
-azimuth = 35
-freqs = [400, 500, 600, 700, 800]
+subject = "kemar_inner_mic"
+azimuth = 0
+freqs = [400, 600, 800, 1000]
 
 SAMPLE_RATE = 48828
 
@@ -25,8 +25,7 @@ proc_list = [['RP2', 'RP2',  DIR/'rcx'/'bi_rec_buf.rcx'],
 freefield.initialize('dome', zbus=True, device=proc_list)
 
 
-def play_and_record_local(sound):
-    speaker = 23
+def play_and_record_local(sound, speaker):
     freefield.write(tag="playbuflen", value=sound.n_samples, processors=["RX81", "RX82"])
     n_delay = freefield.get_recording_delay(play_from="RX8", rec_from="RP2")
     n_delay += 50  # make the delay a bit larger to avoid missing the sound's onset
@@ -41,51 +40,56 @@ def play_and_record_local(sound):
     return rec
 
 
-centre_frequency = 500
-low_cutoff = centre_frequency / (2 ** (1 / 6))
-high_cutoff = centre_frequency * (2 ** (1 / 6))
 
-noise_band_limited = slab.Sound.whitenoise(duration=.5, level=90).filter(frequency=(low_cutoff, high_cutoff), kind="bp").ramp()
-tone = slab.Sound.tone(duration=.5, level=90, frequency=centre_frequency).ramp()
-silence = slab.Sound.silence(duration=.5)
+speaker_ids = [2, 8, 15, 23, 31, 38, 44]
 
-for _ in range(10):
-    noise_broadband = slab.Sound.whitenoise(duration=.5, level=90).ramp()
-    sound = noise_broadband
-    sound.level = 80
+for speaker_id in speaker_ids:
+    speaker = freefield.pick_speakers(speaker_id)[0]
+    azimuth = speaker.azimuth
+    for centre_frequency in freqs:
+        low_cutoff = centre_frequency / (2 ** (1 / 6))
+        high_cutoff = centre_frequency * (2 ** (1 / 6))
+        for _ in range(10):
+            noise_broadband = slab.Sound.whitenoise(duration=.5, level=90).ramp()
+            noise_band_limited = slab.Sound.whitenoise(duration=.5, level=90).filter(frequency=(low_cutoff, high_cutoff),
+                                                                                     kind="bp").ramp()
+            sound = noise_band_limited
+            sound.level = 90
 
-    rec = play_and_record_local(sound)
-    folder = "recordings"
-    filename = '_'.join(filter(None, (subject, "azi", str(azimuth))))
-    filepath = pathlib.Path(folder / pathlib.Path(subject) / str("azi_" + str(azimuth)) / pathlib.Path(filename +
-                                                                          datetime.now().strftime(
-                                                                              "_%Y-%m-%d-%H-%M-%S") + '.wav'))
-    filepath.parent.mkdir(parents=True, exist_ok=True)
-    rec.write(filepath)
-    time.sleep(0.1)
+            rec = play_and_record_local(sound, speaker)
+            folder = "recordings"
+            filename = '_'.join(filter(None, (subject, "azi", str(azimuth), "freq", str(centre_frequency))))
+            filepath = pathlib.Path(folder / pathlib.Path(subject) / str("azi_" + str(azimuth)) / str(centre_frequency) / pathlib.Path(filename +
+                                                                                  datetime.now().strftime(
+                                                                                      "_%Y-%m-%d-%H-%M-%S") + '.wav'))
+            filepath.parent.mkdir(parents=True, exist_ok=True)
+            rec.write(filepath, normalise=False)
+            time.sleep(0.1)
 
 DIR_recs = DIR / "recordings"
 subjects = os.listdir(DIR_recs)
 subjects = [subject]
 
-df_binaural_measurements = pd.DataFrame()
+# df_binaural_measurements = pd.DataFrame()
+# freqs = [centre_frequency]
 
 for subject in subjects:
     for azi in os.listdir(DIR_recs / subject):
-        for i, filename in enumerate(os.listdir(DIR_recs / subject / azi)):
-            rec = slab.Binaural(DIR_recs / subject / azi / filename)
-            azimuth_int = int(azi[4:])
-            for freq in freqs:
-                low_cutoff = freq / (2 ** (1 / 6))
-                high_cutoff = freq * (2 ** (1 / 6))
+        for freq in os.listdir(DIR_recs / subject / azi):
+            for i, filename in enumerate(os.listdir(DIR_recs / subject / azi / freq)):
+                rec = slab.Binaural(DIR_recs / subject / azi / freq / filename)
+                azimuth_float = float(azi[4:])
+                freq_float = float(freq)
+                low_cutoff = freq_float / (2 ** (1 / 6))
+                high_cutoff = freq_float * (2 ** (1 / 6))
                 rec = rec.filter(frequency=(low_cutoff, high_cutoff), kind="bp")
                 itd = rec.itd() / rec.samplerate
                 ild = rec.ild()
                 binaural_current = {
                     "filename": filename,
                     "subject": subject,
-                    "azimuth": azimuth_int,
-                    "freq": freq,
+                    "azimuth": azimuth_float,
+                    "freq": freq_float,
                     "itd": itd,
                     "ild": ild,
                     "rms": rec.level.mean(),
@@ -102,15 +106,21 @@ df_binaural_measurements.groupby(["subject", "freq", "azimuth"])["ild", "itd"].m
 g = sns.FacetGrid(data=df_binaural_measurements, row="freq", col="subject", sharey=False)
 g.map(sns.lineplot, "azimuth", "rms_left", marker="o", color="g")
 g.map(sns.lineplot, "azimuth", "rms_right", marker="o", color="r")
-g.map(sns.lineplot, "azimuth", "ild", marker="o", color="b")
+# g.map(sns.lineplot, "azimuth", "ild", marker="o", color="b")
 plt.show()
 
-g = sns.FacetGrid(data=df_binaural_measurements, row="freq", col="subject")
-g.map(sns.lineplot, "azimuth", "ild", marker="o", color="b")
+g = sns.FacetGrid(data=df_binaural_measurements, hue="freq", col="subject", palette="copper")
+g.map(sns.lineplot, "azimuth", "ild", marker="o")
 plt.show()
 
-g = sns.FacetGrid(data=df_binaural_measurements, hue="freq", col="subject")
+g = sns.FacetGrid(data=df_binaural_measurements, hue="freq", col="subject", palette="copper")
 g.map(sns.lineplot, "azimuth", "itd", marker="o")
+g.add_legend()
+plt.show()
+
+g = sns.FacetGrid(data=df_binaural_measurements[df_binaural_measurements.freq < 700], hue="subject")
+g.map(sns.lineplot, "azimuth", "itd", marker="o")
+g.add_legend()
 plt.show()
 
 df_sub = df_binaural_measurements[df_binaural_measurements.subject == subject]

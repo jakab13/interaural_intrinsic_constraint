bbfa4b6d80ebaa4bc080b480b275295cc91cd532
Kirkem <kirke.muller@freenet.de>
2025-01-22 12:13:54 +0100

    width

--- analysis.py
+++ analysis.py
@@ -4,3 +4,21 @@
 #       use the psignifit library for the calculations
 # store the results (mean, slope) of the psychometric fit in the same data sheet
 # plot psychometric functions in a Facetgrid using the seaborn library
+
+
+#6. plot width against frequencies for all subjects in one plot, each width is one point
+palette= {'ILD-->ILD':'C1','ITD-->ITD':'C0','ILD-->ITD':'C2','ITD-->ILD':'C3'}
+
+sns.lineplot(    #lineplot
+    data=df_model,
+    x='standard_center_frequency',
+    y='width',
+    style='subject',
+    hue='trial_type',
+    err_style='bars',
+    errorbar='ci',
+    marker='o',
+    palette=palette
+)
+plt.savefig('/home/kirke/Schreibtisch/ma/python/figures/width')
+plt.show()
